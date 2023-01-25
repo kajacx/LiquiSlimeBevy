e8f3bccaaf1b340dad9d86873e5419e6e49eadde
kajacx <kajacx@gmail.com>
2023-01-25 16:10:13 +0100

    Progress...

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{ecs::system::Command, prelude::*, sprite::MaterialMesh2dBundle};
-use components::{Building, SlimeAmount, TilePosition};
+use components::{Building, SlimeAmount, SlimeGrid, TilePosition};
 
 mod components;
 
@@ -8,18 +8,20 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_startup_system(spawn_tiles(10, 10))
-        .add_system(increase_slime)
-        .add_system(render_slime_color)
-        .add_system(prepare_slime_spread)
-        .add_system(spread_slime)
+        .add_system_to_stage(CoreStage::Update, spread_slime)
+        .add_system_to_stage(CoreStage::Last, render_slime_color)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
+
     let scale = 0.02f32;
+    let position = 5f32;
+
     camera.transform = Transform::from_scale(Vec3::new(scale, scale, scale));
-    camera.transform.translation = Vec3::new(5f32, 5f32, 0f32);
+    camera.transform.translation = Vec3::new(position, position, 0f32);
+
     commands.spawn(camera);
 }
 
@@ -44,42 +46,27 @@ fn spawn_tiles(width: usize, height: usize) -> impl Fn(Commands) {
                 commands.spawn((position, amount, sprite));
             }
         }
+
+        let slime_grid = SlimeGrid::new(width, height);
+        commands.spawn(slime_grid);
     }
 }
 
-fn increase_slime(mut query: Query<&mut SlimeAmount>) {
-    for mut amount in &mut query {
-        amount.0 += 1;
-    }
-}
-
-fn render_slime_color(mut query: Query<(&mut Sprite, &SlimeAmount)>) {
-    for (mut color, amount) in &mut query {
+fn render_slime_color(
+    mut tile_query: Query<&mut Sprite, With<Tile>>,
+    grid_query: Query<SlimeGrid>,
+) {
+    let slime_grid = grid_query.single();
+    for mut sprite_tile in &mut tile_query {
+        let pos = sprite_tile.transform.position;
+        let amount = slime_grid.get_amount(pos.get_x(), pos.get_y());
         let rgb = amount.0 as u8;
-        color.color = Color::rgb_u8(rgb, rgb, rgb);
+        sprite_tile.color = Color::rgb_u8(rgb, rgb, rgb);
     }
 }
 
-fn prepare_slime_spread(query: Query<(&Tile, &WithNeighbor)>) {
-    for (tile, with_neighbor) in &query {
-        let neighbor = with_neighbor.neighbor;
-
-        let current_slime = tile.slime_amount.0;
-        let neighbor_slime = neighbor.slime_amount.0;
-
-        // Moved slime from current to neighbor
-        let moved_slime = current_slime - neighbor_slime / 12; // TODO: spread speed factor
-
-        tile.incoming_slime
-            .set(tile.incoming_slime.get() - moved_slime);
-        neighbor_slime
-            .incoming_slime
-            .set(neighbor_slime.incoming_slime.get() + moved_slime);
-    }
-}
-
-fn spread_slime(mut query: Query<&mut Tile>) {
-    for mut tile in &mut query {
-        tile.slime_amount += tile.incoming_slime.replace(0);
-    }
+fn spread_slime(mut query: Query<&mut SlimeGrid>) {
+    let slime_grid = query.single_mut();
+    slime_grid.prepare_slime_spread();
+    slime_grid.spread_slime();
 }
