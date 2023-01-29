0ae76ba2fa7dfdedcaf92b16114480a6c8b4d5a8
kajacx <kajacx@gmail.com>
2023-01-29 17:07:38 +0100

    Try get world position click

--- src/components/tile_position.rs
+++ src/components/tile_position.rs
@@ -10,4 +10,11 @@ impl TilePosition {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
+
+    pub fn from_floats_floor(x: f32, y: f32) -> Self {
+        Self {
+            x: x.floor() as i32,
+            y: y.floor() as i32,
+        }
+    }
 }
--- src/systems/input.rs
+++ src/systems/input.rs
@@ -1,4 +1,4 @@
-use bevy::{input::mouse::MouseButtonInput, prelude::*};
+use bevy::{input::mouse::MouseButtonInput, prelude::*, render::camera::RenderTarget};
 
 use crate::components::{Building, MoveOnClick, SlimeGrid, Tile, TilePosition};
 
@@ -13,10 +13,51 @@ impl Plugin for GameInputPlugin {
 fn move_sources(
     mut spawners: Query<(&mut TilePosition, &MoveOnClick)>,
     mut mouse_input: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    //camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>, // TODO: handle multiple cameras?
+    camera: Query<(&Camera, &GlobalTransform)>,
 ) {
-    for (mut spawner, move_on) in &mut spawners {
-        if mouse_input.just_pressed(move_on.mouse_button) {
-            spawner.x = (spawner.x + 1) % 10; // TODO: hardwired level width
+    // get the camera info and transform
+    // assuming there is exactly one main camera entity, so query::single() is OK
+    let (camera, camera_transform) = camera.get_single().expect("Camera should exist");
+
+    // get the window that the camera is displaying to (or the primary window)
+    let window = if let RenderTarget::Window(id) = camera.target {
+        windows.get(id)
+    } else {
+        windows.get_primary()
+    }
+    .expect("Window should be found");
+
+    // check if the cursor is inside the window and get its position
+    if let Some(screen_pos) = window.cursor_position() {
+        // get the size of the window
+        let window_size = Vec2::new(window.width() as f32, window.height() as f32);
+
+        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+        let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+
+        // matrix for undoing the projection and camera transform
+        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+
+        // use it to convert ndc to world-space coordinates
+        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+
+        // reduce it to a 2D value
+        let world_pos: Vec2 = world_pos.truncate();
+
+        eprintln!("World coords: {}/{}", world_pos.x, world_pos.y);
+
+        let tile_pos = TilePosition::from_floats_floor(world_pos.x, world_pos.y);
+
+        // Game logic once we have the world coords
+        if (tile_pos.x >= 0 && tile_pos.x < 10 && tile_pos.y >= 0 && tile_pos.y < 10) {
+            // TODO: hardwired world side. Also: make a new method "in world" in tile position?
+            for (mut spawner_position, move_on) in &mut spawners {
+                if mouse_input.just_pressed(move_on.mouse_button) {
+                    *spawner_position = tile_pos;
+                }
+            }
         }
     }
 }
