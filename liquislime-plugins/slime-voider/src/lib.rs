82effc6e8bf62da37b5ed061a354c6b7ce107930
kajacx <kajacx@gmail.com>
2023-02-12 15:18:40 +0100

    Add slime voider plugin

--- liquislime-plugins/slime-voider/src/lib.rs
+++ liquislime-plugins/slime-voider/src/lib.rs
@@ -2,13 +2,13 @@ use liquislime_api::*;
 
 #[fp_export_impl(liquislime_api)]
 fn update(time_elapsed: TimeInterval) {
-    if was_mouse_just_pressed(MouseButton::LeftButton) {
+    if was_mouse_just_pressed(MouseButton::RightButton) {
         set_own_position(get_mouse_position().to_tile_position());
     }
 
-    let added_amount_per_second = SlimeAmount::from_integer(1000);
+    let removed_amount_per_second = SlimeAmount::from_integer(1000);
 
     let amount = get_slime_amount(get_own_position());
-    let amount = amount + added_amount_per_second * time_elapsed.to_seconds();
+    let amount = amount - removed_amount_per_second * time_elapsed.to_seconds();
     set_slime_amount(get_own_position(), amount);
 }
