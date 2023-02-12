fd5faab3dda89c2fbc1309d726fdeddfda10b1b1
kajacx <kajacx@gmail.com>
2023-02-12 12:31:03 +0100

    Make run script copy entire type definitions

--- run-dev.sh
+++ run-dev.sh
@@ -4,6 +4,11 @@ echo "Building API protocol" && \
 cd liquislime-api && \
 cargo test && \
 cargo run && \
+rm bindings/rust-plugin/src/types.rs && \
+cp -r src/types bindings/rust-plugin/src/types && \
+rm bindings/rust-wasmer-runtime/types.rs && \
+cp -r src/types bindings/rust-wasmer-runtime/types && \
+cd bindings/rust-plugin && cargo check && cd .. && \
 cd .. && \
 echo "API protocol built" && \
 \
