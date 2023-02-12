24c91cd43b75bf5c8d0c590afa6c0414c2cb1d8d
kajacx <kajacx@gmail.com>
2023-02-12 12:47:08 +0100

    Fix run script

--- run-dev.sh
+++ run-dev.sh
@@ -4,10 +4,13 @@ echo "Building API protocol" && \
 cd liquislime-api && \
 cargo test --features api-generation && \
 cargo run --features api-generation && \
-rm bindings/rust-plugin/src/types.rs && \
+\
+rm -r bindings/rust-plugin/src/types* && \
 cp -r src/types bindings/rust-plugin/src/types && \
-rm bindings/rust-wasmer-runtime/types.rs && \
+\
+rm -r bindings/rust-wasmer-runtime/types* && \
 cp -r src/types bindings/rust-wasmer-runtime/types && \
+\
 cd bindings/rust-plugin && cargo check && cd .. && \
 cd .. && \
 echo "API protocol built" && \
