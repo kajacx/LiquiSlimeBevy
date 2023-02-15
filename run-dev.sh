3654d42baedef98873efd84b9767b1321485c48a
kajacx <kajacx@gmail.com>
2023-02-15 20:13:06 +0100

    Add Component to API types

--- run-dev.sh
+++ run-dev.sh
@@ -5,10 +5,10 @@ cd liquislime-api && \
 cargo test --features api-generation && \
 cargo run --features api-generation && \
 \
-rm -r bindings/rust-plugin/src/types* && \
+rm -rf          bindings/rust-plugin/src/types* && \
 cp -r src/types bindings/rust-plugin/src/types && \
 \
-rm -r bindings/rust-wasmer-runtime/types* && \
+rm -rf          bindings/rust-wasmer-runtime/types* && \
 cp -r src/types bindings/rust-wasmer-runtime/types && \
 \
 cd bindings/rust-plugin && cargo check && cd ../.. && \
@@ -25,9 +25,15 @@ done && \
 cd .. && \
 echo "Plugins built" && \
 \
+echo "Copying bindings to bevy host" && \
+rm -rf                                                        liquislime-bevy/src/units/api_spec/types && \
+cp -r liquislime-api/bindings/rust-wasmer-runtime/types       liquislime-bevy/src/units/api_spec/ && \
+cp    liquislime-api/bindings/rust-wasmer-runtime/bindings.rs liquislime-bevy/src/units/api_spec/bindings.rs && \
+echo "Bindings copied" && \
+\
 echo "Running bevy game in debug mode" && \
 cd liquislime-bevy && \
-cargo run && \
+cargo run --features bevy-host && \
 cd .. && \
 \
 echo "All done"
