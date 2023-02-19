639a4567044bcf9d3cfad2ba903ff0d937adbb36
kajacx <kajacx@gmail.com>
2023-02-19 15:04:15 +0100

    Make world reference (more) safe

--- liquislime-bevy/src/units/global_storage/world_holder.rs
+++ liquislime-bevy/src/units/global_storage/world_holder.rs
@@ -1,33 +1,61 @@
 use bevy::prelude::*;
-use std::{marker::PhantomData, ops::DerefMut, sync::Mutex};
+use std::{
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+    sync::{Mutex, MutexGuard},
+};
 
-static GLOBAL_WORLD: Mutex<UnsafeWorldRef> = Mutex::new(UnsafeWorldRef(std::ptr::null_mut()));
+static GLOBAL_WORLD: Mutex<InnerWorldRef> = Mutex::new(InnerWorldRef(std::ptr::null_mut()));
+static THREAD_LOCK: Mutex<()> = Mutex::new(());
 
 fn set_world(world: *mut World) {
-    (*GLOBAL_WORLD.lock().expect("Set world mutex lock")).0 = world;
+    (*GLOBAL_WORLD.try_lock().expect("Set world mutex lock")).0 = world;
 }
 
 pub fn get_world() -> impl DerefMut<Target = World> {
-    let locked = GLOBAL_WORLD.lock().expect("Get world mutex lock");
-    let inner = locked.0;
-
-    if inner.is_null() {
-        panic!("Global world reference is null.");
-    }
-
-    /* SAFETY:
-     * Setting the world referene is not public. The only way to set it is via the `use_world_reference_in` function.
-     * That takes the mutable reference to the world, so no one else can use it while the function runs.
-     * And it is only while the function run that this code will be reached, because of the null check.
-     * Finally, you cannot call get_world() twice to get 2 reference, because of the mutex lock.
-     */
-    unsafe { &mut *inner }
-
-    // TODO: this is still unsafe!
+    let locked = GLOBAL_WORLD.try_lock().expect("Get world mutex lock");
+    WorldRef(locked)
 }
 
-struct UnsafeWorldRef(*mut World);
-unsafe impl Send for UnsafeWorldRef {}
+struct InnerWorldRef(*mut World);
+unsafe impl Send for InnerWorldRef {}
+
+struct WorldRef<'a>(MutexGuard<'a, InnerWorldRef>);
+
+impl Deref for WorldRef<'_> {
+    type Target = World;
+
+    fn deref<'a>(&'a self) -> &'a Self::Target {
+        let inner_pointer = self.0 .0;
+
+        if inner_pointer.is_null() {
+            panic!("Global world reference is null.");
+        }
+
+        // SAFETY: shared reference to self ensures that no mutable reference exists.
+        // See the DerefMut impl for the rest of the safety arguments.
+        unsafe { &*inner_pointer }
+    }
+}
+
+impl DerefMut for WorldRef<'_> {
+    fn deref_mut<'a>(&'a mut self) -> &'a mut Self::Target {
+        let inner_pointer = self.0 .0;
+
+        if inner_pointer.is_null() {
+            panic!("Global world reference is null.");
+        }
+
+        /* SAFETY:
+         * Setting the world referene is not public. The only way to set it is via the `use_world_reference_in` function.
+         * That takes the mutable reference to the world, so no one else can use it while the function runs.
+         * And it is only while the function run that this code will be reached, because of the null check.
+         * You also cannot call the `use_world_reference_in` twice at the same time because of the THREAD_LOCK.
+         * Finally, you cannot call get_world() twice to get 2 references, because of the mutex lock.
+         */
+        unsafe { &mut *inner_pointer }
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct WorldRefToken {
@@ -35,6 +63,10 @@ pub struct WorldRefToken {
 }
 
 pub fn use_world_reference_in(reference: &mut World, use_in: impl FnOnce(&WorldRefToken)) {
+    let lock = THREAD_LOCK
+        .try_lock()
+        .expect("You should only call this method once at a time");
+
     set_world(reference as *mut _);
 
     let ref_token = WorldRefToken {
@@ -44,4 +76,6 @@ pub fn use_world_reference_in(reference: &mut World, use_in: impl FnOnce(&WorldR
     use_in(&ref_token);
 
     set_world(std::ptr::null_mut());
+
+    drop(lock);
 }
