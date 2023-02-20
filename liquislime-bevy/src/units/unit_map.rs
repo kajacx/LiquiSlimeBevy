495fc4c1641a6ee902e940f9669b5730aaef6826
kajacx <kajacx@gmail.com>
2023-02-20 19:46:16 +0100

    Fix code so that it at least compiles

--- liquislime-bevy/src/units/unit_map.rs
+++ liquislime-bevy/src/units/unit_map.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, ops::DerefMut, sync::Mutex};
+use std::{
+    collections::HashMap,
+    ops::DerefMut,
+    sync::{Arc, Mutex},
+};
 
 use bevy::render::once_cell::sync::Lazy;
 
@@ -8,19 +12,10 @@ use super::{global_storage::WorldRefToken, script::Script, UnitId};
 
 type UnitMap = HashMap<UnitId, Script>;
 
-static UNIT_SCRIPT_MAP: Lazy<Mutex<UnitMap>> = Lazy::new(|| Mutex::new(HashMap::new()));
-
 pub fn register_new_unit(id: UnitId, script: Script) {
-    (&mut *get_map()).insert(id, script);
+    todo!()
 }
 
 pub fn update_all_units(time_elapsed: TimeInterval, _world_ref: &WorldRefToken) {
-    for (unit_id, script) in get_map().iter() {
-        set_current_unit(unit_id.clone()); // TODO: how to copy a copy type?
-        script.update(time_elapsed);
-    }
-}
-
-fn get_map() -> impl DerefMut<Target = UnitMap> {
-    UNIT_SCRIPT_MAP.lock().expect("Should lock unit script map")
+    todo!()
 }
