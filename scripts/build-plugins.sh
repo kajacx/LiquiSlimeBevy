5810c1f922060bfab37d6d3ff0572ba1523272f9
kajacx <kajacx@gmail.com>
2023-02-21 19:01:31 +0100

    Copy plugins to assets

--- scripts/build-plugins.sh
+++ scripts/build-plugins.sh
@@ -5,6 +5,7 @@ cd liquislime-plugins && \
 for plugin in */; do
   cd $plugin && \
   cargo build --target wasm32-unknown-unknown && \
+  cp target/wasm32-unknown-unknown/debug/*.wasm ../../liquislime-bevy/assets/plugins/
   cd ..
 done && \
 cd .. && \
