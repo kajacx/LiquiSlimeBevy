af2419fbeb5dead6e59a1416cd414ffb7e5902d5
kajacx <kajacx@gmail.com>
2023-03-02 22:36:01 +0100

    Fix default js engine

--- run-web.sh
+++ run-web.sh
@@ -6,8 +6,9 @@
 \
 echo "Running bevy game in web browser" && \
 cd liquislime-bevy/src/units/api_spec && \
-head -n -1 bindings-web.rs > bindings-fixed.rs && \
-tail -n +47 bindings.rs >> bindings-fixed.rs && \
+head -n -1 bindings-web-wasmer3.rs > bindings-fixed.rs && \
+#tail -n +47 bindings.rs >> bindings-fixed.rs && \
+tail -n +38 bindings.rs >> bindings-fixed.rs && \
 mv bindings-fixed.rs bindings.rs && \
 cd ../../.. && \
 cargo run --features bevy-host --target=wasm32-unknown-unknown && \
