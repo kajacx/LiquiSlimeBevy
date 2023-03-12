d7ddeedad43683e84036cc88d861e1e504770135
kajacx <kajacx@gmail.com>
2023-03-12 17:33:04 +0100

    Move web assets to "webserver" folder

--- run-web.sh
+++ run-web.sh
@@ -9,13 +9,15 @@ cd liquislime-bevy && \
 #cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456 && \
 #cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456 && \
 cargo build --target=wasm32-unknown-unknown && \
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm && \
+wasm-bindgen --out-dir ./webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm && \
 cd .. && \
-echo "Bevy game build in WASM" && \
+echo "Bevy game built in WASM" && \
 \
 echo "Restarting webserver" && \
 cd liquislime-bevy/webserver && \
+rm -rf assets && \
+cp -r ../assets . && \
 docker-compose down && docker-compose up -d && \
 cd ../.. && \
 \
-echo "All done, play the game at http://127.0.0.1:8088/out/"
+echo "All done, play the game at http://127.0.0.1:8088/webserver/"
