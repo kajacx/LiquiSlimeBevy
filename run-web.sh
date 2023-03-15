4cd4b31309a17c7601ad7a1c705424cf0f59d241
kajacx <kajacx@gmail.com>
2023-03-15 19:49:39 +0100

    Web script "working" again

--- run-web.sh
+++ run-web.sh
@@ -6,8 +6,8 @@
 \
 echo "Running bevy game in web browser" && \
 cd liquislime-bevy && \
-#cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456 && \
-#cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456 && \
+# cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456 && \
+# cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456 && \
 cargo build --target=wasm32-unknown-unknown && \
 wasm-bindgen --out-dir ./webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm && \
 cd .. && \
