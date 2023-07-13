1906596fed640b04de38c0c3353a44df3a5b54f3
kajacx <kajacx@gmail.com>
2023-07-13 10:53:08 +0200

    Run web game in separate project to speed up build time

--- run-web.sh
+++ run-web.sh
@@ -3,21 +3,29 @@ set -e
 
 ./scripts/build-plugins.sh
 
+echo "Copying liquislime source"
+rm -rf liquislime-web/.cargo
+rm -rf liquislime-web/src
+rm -rf liquislime-web/Cargo.toml
+cp -r liquislime-bevy/.cargo liquislime-web/
+cp -r liquislime-bevy/src liquislime-web/
+cp -r liquislime-bevy/Cargo.toml liquislime-web/
+
+echo "Copying liquislime assets"
+rm -rf liquislime-web/liquislime-webserver/assets
+cp -r liquislime-bevy/assets liquislime-web/liquislime-webserver/
+
 echo "Running bevy game in web browser"
-cd liquislime-bevy
+cd liquislime-web
 # cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456
 # cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456
 cargo build --target=wasm32-unknown-unknown
 wasm-bindgen --out-dir ./webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm
 
-cd ..
 echo "Bevy game built in WASM"
 
 echo "Restarting webserver"
-cd liquislime-bevy/webserver
-rm -rf assets
-cp -r ../assets .
+cd liquislime-webserver
 docker-compose down && docker-compose up -d
-cd ../..
 
-echo "All done, play the game at http://127.0.0.1:8088/webserver/"
+echo "All done, play the game at http://127.0.0.1:8088/"
