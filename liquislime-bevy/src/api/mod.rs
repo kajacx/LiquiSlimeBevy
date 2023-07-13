edb11dba389619b2aa99a59cc142c603fbecb5ee
kajacx <kajacx@gmail.com>
2023-07-13 10:59:54 +0200

    Remove old code

--- liquislime-bevy/src/api/mod.rs
+++ liquislime-bevy/src/api/mod.rs
@@ -2,11 +2,5 @@
 mod types;
 pub use types::*;
 
-// mod wasmtime_impl;
-// pub use wasmtime_impl::*;
-
-// mod jco_impl;
-// pub use jco_impl::*;
-
 mod wasm_bridge_impl;
 pub use wasm_bridge_impl::*;
