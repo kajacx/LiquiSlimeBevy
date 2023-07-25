5348697cfb18bfdd56b165f54f506b11a7a13d4c
kajacx <kajacx@gmail.com>
2023-07-25 21:20:47 +0200

    Update ubuntu web script

--- scripts/run-ubuntu-web.sh
+++ scripts/run-ubuntu-web.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/sh
+set -e
 
-echo "TODO: refactor this script"
-exit 1
+# Run from the parent folder
 
-sh ./scripts/build-api.sh && \
-sh ./scripts/build-plugins.sh && \
-sh ./scripts/prepare-host.sh && \
-\
-echo "Running bevy game in web browser" && \
-cd liquislime-bevy && \
-# cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456 && \
-# cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456 && \
-cargo build -Zbuild-std=std,panic_abort --target=wasm32-unknown-unknown && \
-wasm-bindgen --out-dir ./webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm && \
-cd .. && \
-echo "Bevy game built in WASM" && \
-\
-echo "Restarting webserver" && \
-cd liquislime-bevy/webserver && \
-rm -rf assets && \
-cp -r ../assets . && \
-sudo docker-compose down && sudo docker-compose up -d && \
-cd ../.. && \
-\
+sh scripts/build-plugins.sh
+sh scripts/start-web.sh --release
+
+echo "Restarting webserver"
+cd liquislime-web/liquislime-webserver
+sudo docker-compose down && sudo docker-compose up -d
+cd ../..
+
 echo "All done, play the game at http://liquislime.com:8088/webserver/"
--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Run from parent folder
+mode="$1"
 
 echo "Copying liquislime source"
 rm -rf liquislime-web/.cargo
@@ -19,8 +20,13 @@ echo "Running bevy game in web browser"
 cd liquislime-web
 # cargo run --target=wasm32-unknown-unknown -- --initial-memory=268435456 --max-memory=268435456
 # cargo rustc --target=wasm32-unknown-unknown -- -Clink-arg=--initial-memory=268435456 -Clink-arg=--max-memory=268435456
-cargo build --target=wasm32-unknown-unknown
-wasm-bindgen --out-dir ./liquislime-webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm
+
+cargo build "$mode" --target=wasm32-unknown-unknown
+[ "$mode" = "--release" ]; if
+  wasm-bindgen --out-dir ./liquislime-webserver/ --target web ./target/wasm32-unknown-unknown/release/liquislime-bevy.wasm
+else
+  wasm-bindgen --out-dir ./liquislime-webserver/ --target web ./target/wasm32-unknown-unknown/debug/liquislime-bevy.wasm
+fi
 
 echo "Bevy game built in WASM"
 
