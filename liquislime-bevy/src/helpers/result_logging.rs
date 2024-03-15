69aea02d6f4fe47df8575e5970da47e52c7ff7be
kajacx <kajacx@gmail.com>
2024-03-15 19:33:44 +0100

    Update error logging

--- liquislime-bevy/src/helpers/result_logging.rs
+++ liquislime-bevy/src/helpers/result_logging.rs
@@ -1,66 +1,56 @@
 use std::fmt::Debug;
 
-use bevy::prelude::warn;
-
 pub trait ResultLogger<R> {
-    fn log_err(self);
+    fn log_err(self, msg: &str);
 
-    fn log_err_or(self, or: R) -> R;
+    fn log_err_or(self, or: R, msg: &str) -> R;
 
-    fn log_err_or_else(self, or_else: impl FnOnce() -> R) -> R;
+    fn log_err_or_else(self, or_else: impl FnOnce() -> R, msg: &str) -> R;
 }
 
 impl<R, E> ResultLogger<R> for Result<R, E>
 where
     E: Debug,
 {
-    fn log_err(self) {
+    fn log_err(self, msg: &str) {
         if let Err(err) = self {
-            print_err(err);
+            tracing::warn!("{msg}: {err:?}");
         }
     }
 
-    fn log_err_or(self, or: R) -> R {
-        self.log_err_or_else(move || or)
+    fn log_err_or(self, or: R, msg: &str) -> R {
+        self.log_err_or_else(move || or, msg)
     }
 
-    fn log_err_or_else(self, or_else: impl FnOnce() -> R) -> R {
+    fn log_err_or_else(self, or_else: impl FnOnce() -> R, msg: &str) -> R {
         match self {
             Ok(value) => value,
             Err(err) => {
-                print_err(err);
+                tracing::warn!("{msg}: {err:?}");
                 or_else()
             }
         }
     }
-}
-
-fn print_err(error: impl Debug) {
-    warn!("Logging result error: {:?}", error);
 }
 
 impl<T> ResultLogger<T> for Option<T> {
-    fn log_err(self) {
+    fn log_err(self, msg: &str) {
         if self.is_none() {
-            print_none();
+            tracing::warn!("{msg}");
         }
     }
 
-    fn log_err_or(self, or: T) -> T {
-        self.log_err_or_else(move || or)
+    fn log_err_or(self, or: T, msg: &str) -> T {
+        self.log_err_or_else(move || or, msg)
     }
 
-    fn log_err_or_else(self, or_else: impl FnOnce() -> T) -> T {
+    fn log_err_or_else(self, or_else: impl FnOnce() -> T, msg: &str) -> T {
         match self {
             Some(value) => value,
             None => {
-                print_none();
+                tracing::warn!("{msg}");
                 or_else()
             }
         }
     }
-}
-
-fn print_none() {
-    warn!("Option was none");
 }
