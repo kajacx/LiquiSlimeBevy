b9d6f86d3dac9d5576f78781b73b7daf3975497c
kajacx <kajacx@gmail.com>
2024-06-14 12:26:43 +0200

    Settings macro fix

--- liquislime-scripts/liquislime-settings/src/lib.rs
+++ liquislime-scripts/liquislime-settings/src/lib.rs
@@ -1,42 +1,4 @@
-use crate::api::ToWasmAbi;
-use anyhow::Result;
-use plugin::UserScript;
-use settings::{SettingsDescription, SettingsValue};
-use std::{collections::HashMap, fmt::Display};
-use try_lock::TryLock;
-
-#[allow(unused)]
-mod types;
-#[allow(unused)]
-use types::*;
-
-mod plugin;
-
-mod settings;
-
-mod api;
-
-static INSTANCES: TryLock<Option<HashMap<Instance, UserScript>>> = TryLock::new(None);
-
-pub trait SettingsTemplate: Sized {
-    fn describe_settings() -> SettingsDescription;
-
-    fn default_value() -> SettingsValue;
-
-    fn parse(value: SettingsValue) -> Result<Self>;
-}
-
-pub trait ScriptTemplate {
-    type Settings: SettingsTemplate;
-
-    fn new_instance(settings: Self::Settings) -> Self;
-
-    fn change_settings(&mut self, settings: Self::Settings);
-
-    fn update(&mut self, time_elapsed: TimeInterval);
-}
-
-#[allow(unused)]
-fn log(msg: impl Display) {
-    unsafe { api::log((&*format!("{msg}")).to_wasm_abi()) }
+#[proc_macro_derive(LiquislimeSettings)]
+pub fn derive_settings(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    proc_macro::TokenStream::new()
 }
