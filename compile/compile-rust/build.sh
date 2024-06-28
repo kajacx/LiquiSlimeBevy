f4f0243b4d30f25e7de90fe39b5c88021b425ae5
Karel Hrkal <kajacx@gmail.com>
2024-06-28 13:01:49 +0200

    Try add rust compile docker

--- compile/compile-rust/build.sh
+++ compile/compile-rust/build.sh
@@ -8,3 +8,15 @@ cp -r liquislime-scripts/rust/rust-template compile/compile-rust/user-plugin
 cd compile/compile-rust
 
 docker build -t liquislime/compile-rust .
+
+# test that it works
+# docker container stop compile-rust-test || true
+# docker container rm compile-rust-test || true
+
+# docker volume create --name compile-rust-target
+# docker run --name compile-rust-test -v rust-compile-target:/target -p 8001:8000 -d liquislime/compile-rust
+
+docker compose down
+docker compose up -d
+
+
